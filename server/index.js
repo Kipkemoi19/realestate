c968a7065b587a74d7fa13c591484c7d09feb6a7
Deborah Chebii <chebiideborah@gmail.com>
2024-12-29 22:03:26 +0300

    o

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 // Declare PORT with const
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser()); // Fixed spelling
