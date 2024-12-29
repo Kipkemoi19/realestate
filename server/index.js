1046f7600782ae6693a03bb69335796d8c47dd1a
Deborah Chebii <chebiideborah@gmail.com>
2024-12-29 21:02:36 +0300

    commplete

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser"; // Fixed spelling
+import cors from "cors";
+import { userRoute } from './routes/userRoute.js';
+import { residencyRoute } from './routes/residencyRoute.js';
+dotenv.config();
+
+const app = express();
+
+// Declare PORT with const
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cookieParser()); // Fixed spelling
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log(`Server is running on Port ${PORT}`); // Fixed typo
+});
+
+app.use('/api/user', userRoute)
+app.use("/api/residency", residencyRoute)
