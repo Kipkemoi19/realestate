b9b8410fbcba942748e4d3ada036d54853799ca3
Deborah Chebii <chebiideborah@gmail.com>
2024-12-29 22:25:45 +0300

    dd

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://realestate-7oipv8wps-ians-projects-7e780c91.vercel.app/api",
 });
 
 export const getAllProperties = async () => {
